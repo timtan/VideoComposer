0a1d3fea2fcc7c8249a744aa00d431b04c0594d7
tim <tim.yellow@gmail.com>
2016-03-03 13:32:44 +0800

    retab

--- glue.py
+++ glue.py
@@ -9,21 +9,21 @@ height = 240
 pygame.init()
 
 sections = [
-	{
-	'type' : "image",
-	'file': 'lin.jpg',
-	'caption': u"下一任台灣女總統!?",
-	},
-	{
-	'type' : "video",
-	'file' : 's.mpg',
-	'caption': u"wow",
-	},
-	{
-	'type' : "video",
-	'file':'s.mpg',
-	'caption': u"shit",
-	}
+        {
+        'type' : "image",
+        'file': 'lin.jpg',
+        'caption': u"下一任台灣女總統!?",
+        },
+        {
+        'type' : "video",
+        'file' : 's.mpg',
+        'caption': u"wow",
+        },
+        {
+        'type' : "video",
+        'file':'s.mpg',
+        'caption': u"shit",
+        }
 ]
 
 font = pygame.font.Font('MSJH.TTF', 30) 
@@ -35,51 +35,51 @@ screen = pygame.display.set_mode((width, height))
 
 for section in sections:
 
-	section_type = section['type']
+        section_type = section['type']
 
-	if section_type == 'video':
+        if section_type == 'video':
 
-		file_name = section['file']
-		movie = pygame.movie.Movie(file_name)
+                file_name = section['file']
+                movie = pygame.movie.Movie(file_name)
 
-		movie_screen = pygame.Surface((width, height)).convert()
-		movie.set_display(movie_screen)
-		movie.play()
+                movie_screen = pygame.Surface((width, height)).convert()
+                movie.set_display(movie_screen)
+                movie.play()
 
-		while True:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					pygame.quit()  
+                while True:
+                        for event in pygame.event.get():
+                                if event.type == pygame.QUIT:
+                                        pygame.quit()  
 
-			if not movie.get_busy():
-				break
+                        if not movie.get_busy():
+                                break
 
-			screen.blit(movie_screen,(0,0))
-			pygame.display.update()
+                        screen.blit(movie_screen,(0,0))
+                        pygame.display.update()
 
-			clock.tick(FPS)
+                        clock.tick(FPS)
 
-	elif section_type == 'image':
+        elif section_type == 'image':
 
-		for tick in xrange(150):
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					pygame.quit()  
+                for tick in range(150):
+                        for event in pygame.event.get():
+                                if event.type == pygame.QUIT:
+                                        pygame.quit()  
 
-			file_name = section['file']
-			image = pygame.image.load(file_name)
-			image_rect = image.get_rect()
+                        file_name = section['file']
+                        image = pygame.image.load(file_name)
+                        image_rect = image.get_rect()
 
-			caption = section['caption']
+                        caption = section['caption']
 
-			text = font.render(caption, 1, (10, 10, 10))
+                        text = font.render(caption, 1, (10, 10, 10))
 
-			screen.blit(image, (-1* tick,-1*tick))
-			screen.blit(text, (10, 150))
+                        screen.blit(image, (-1* tick,-1*tick))
+                        screen.blit(text, (10, 150))
 
-			pygame.display.update()
+                        pygame.display.update()
 
-			clock.tick(FPS)
+                        clock.tick(FPS)
 
 
 
