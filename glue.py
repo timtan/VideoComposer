7803e1cdb6865c8a244a0bae1687acea902ce1f4
tim <tim.yellow@gmail.com>
2016-03-07 02:26:59 -0800

    it can now play audio during video :)

--- glue.py
+++ glue.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 from __future__ import print_function
 import pygame
+import time
 
 width = 320
 height = 240
@@ -9,6 +10,10 @@ height = 240
 pygame.init()
 
 sections = [
+        {
+            'type' : "audio",
+            'file' : 'sample.ogg',
+            },
         {
             'type' : "image",
             'file': 'lin.jpg',
@@ -19,6 +24,17 @@ sections = [
             'type' : "video",
             'file' : 's.mpg',
             'caption': u"wow",
+            'silence': False,
+            },
+        {
+            'type' : "audio",
+            'file' : 'sample.ogg',
+            },
+        {
+            'type' : "image",
+            'file': 'lin.jpg',
+            'caption': u"下一任台灣女總統!?",
+            'second' : 3, 
             },
         ]
 
@@ -29,31 +45,49 @@ clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((width, height))
 
+
 for section in sections:
 
     section_type = section['type']
 
-    if section_type == 'video':
+    if section_type == 'audio':
+
+        if not pygame.mixer.get_init():
+            print("re init")
+            pygame.mixer.pre_init()
+            pygame.mixer.init()
+            print("after init")
 
         file_name = section['file']
-        pygame.mixer.quit()
+        pygame.mixer.music.load(file_name)
+        pygame.mixer.music.play()
+
+    elif section_type == 'video':
+
+        file_name = section['file']
+        silence = section['silence']
+
+        if not silence:
+            pygame.mixer.quit()
+
         movie = pygame.movie.Movie(file_name)
         movie_screen = pygame.Surface((width, height)).convert()
         movie.set_display(movie_screen)
         movie.play()
 
-        while True:
+        while movie.get_busy():
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()  
 
-            if not movie.get_busy():
-                break
-
             screen.blit(movie_screen,(0,0))
             pygame.display.flip()
-
             clock.tick(FPS)
+
+        if not silence: 
+            # if didn't quit again.  mixer cannot reinit
+            pygame.mixer.quit()
 
     elif section_type == 'image':
 
